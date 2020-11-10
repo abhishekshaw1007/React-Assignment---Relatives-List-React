ca26bf5d32a38dd4eafa1362f88dbb05d436becb
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-11-10 17:19:43 +0530

    Added Oredered List with Keys

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,11 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol key={relativeList}>
+                    <li key={relativeListItem1}>Sweets</li>
+                    <li key={relativeListItem2}>Clothes</li>
+                    <li key={relativeListItem3}>Crackers</li>
+               </ol>
             </div>
         )
     }
